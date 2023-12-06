61af0e34a794e4f34992407ab6ace37afd7c0c1e
omerhussein9 <ohussein@bayviewglen.ca>
2023-12-06 11:34:36 -0500

    finished everything for vue

--- vue3-intro/communicating-events/main.js
+++ vue3-intro/communicating-events/main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
    data() {
       return {
-         cart: 0,
+         cart: [],
          premium: true
       }
    },
-   methods: {}
+   methods: {
+      updateCart(id, adding) {
+         if(adding)
+            this.cart.push(id)
+         else {
+            let index = this.cart.indexOf(id)
+            if(index > -1)
+               this.cart.splice(index, 1)
+            else
+               alert("you can't remove an item that isn't in your cart.")
+         }
+      }
+   }
 })
